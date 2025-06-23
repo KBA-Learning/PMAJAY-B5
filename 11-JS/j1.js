d10f3d79bcc9d78eae7928608a9e1e5e017f7f49
pglekshmi <pglekshmi88@gmail.com>
2025-06-23 14:51:43 +0530

    JS Updated

---
+++ 11-JS/j1.js
@@ -0,0 +1,6 @@
+      console.log("JavaScript");
+      console.log("Sum of 5 & 3",5+3);
+      document.write("Hello World");
+      document.write("<br>The sum of 5 & 3 is &nbsp",5+3)  
+      document.getElementById("text").textContent="This is a session of JavaScript";
+      
---
+++ 11-JS/js3.js
@@ -0,0 +1,5 @@
+let a,b,s;
+        a=10;
+        b=30;
+        s=a+b;
+        document.writeln("The sum is ",s);
