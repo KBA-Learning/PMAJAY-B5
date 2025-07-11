74b3ea2080dd1042976493f5b34d98d67a282249
soyachandracs <soya.chandra@duk.ac.in>
2025-07-11 13:49:03 +0530

    commit

---
+++ 21-Node/Project/index.js
@@ -0,0 +1,7 @@
+import array from "lodash"
+import sum from "./addition.js"
+const a=[1,2,3,4,5]
+let x=5,y=8;
+console.log("Reverse of the aray is ",array.reverse(a));
+console.log("Sum of two numbers:",sum(x,y));
+
---
+++ 21-Node/index.js
@@ -0,0 +1,4 @@
+console.log("Welcome to Javascript");
+let a=10,b=30;
+sum=a+b;
+console.log("sum of two numbers",sum);
